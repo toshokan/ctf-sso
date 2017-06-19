22c58a12101e31125e096d356442f3a0f76b5a4c
toshokan <toshokan@shojigate.net>
2017-06-19 00:55:32 -0400

    Improved error handling and parameter checking

--- web-sso.rb
+++ web-sso.rb
@@ -4,13 +4,16 @@ require 'nokogiri'
 require 'json'
 require 'sinatra'
 
+# Store uris
 $wikiUri = URI("https://tepid.science.mcgill.ca/wiki/index.php/Special:UserLogin")
+$wikiTUri = URI("https://tepid.science.mcgill.ca/wiki/api.php?action=query&format=json&meta=tokens&type=login")
 $tepidUri = URI("http://tepid.science.mcgill.ca/tepid/sessions/")
 $gitblitUri = URI("http://git.sus.mcgill.ca:8080/gitblit/")
 $gitblitQUri = URI("http://git.sus.mcgill.ca:8080/gitblit/?wicket:interface=:1:userPanel:loginForm::IFormSubmitListener::")
 
 
-def wikiAuth(uri, user, pass)
+def wikiAuth(uri, user, pass, params=nil)
+	# Authenticate with Mediawiki
 	cookieJar = HTTP::CookieJar.new
 	res = Net::HTTP.get_response(uri)
 	res.get_fields('Set-Cookie').each do |value|
@@ -25,6 +28,7 @@ def wikiAuth(uri, user, pass)
 		req.form_data = { wpName: user, wpPassword: pass, wploginattempt: 'Log+in', wpEditToken: '+\\', title: 'Special:userLogin', authAction: 'login', force: '', wpLoginToken: loginToken } 
 		req['Cookie'] = HTTP::Cookie.cookie_value(cookieJar.cookies(uri))
 		res = http.request req
+		puts res.body
 		return nil if res.code.to_i == 200
 		res.get_fields('Set-Cookie').each do |value|
 			cookieJar.parse(value, uri)
@@ -69,7 +73,10 @@ get '/' do
 end
 
 post '/login' do
-	wikiJar = wikiAuth($wikiUri, params[:user], params[:pass])
+	if params.nil? || params[:user] == "" || params[:pass] == ""
+		halt 418, '急須です'
+	end
+	wikiJar = wikiAuth($wikiUri, params[:user], params[:pass], TUri: $wikiTUri)
 	gitblitJar = gitblitAuth($gitblitUri, params[:user], params[:pass], QUri: $gitblitQUri)
 	tepidJson = tepidAuth($tepidUri, params[:user], params[:pass])
 
@@ -79,14 +86,30 @@ post '/login' do
 		tepidUserId = JSON.parse(tepidJson)['_id']
 		
 		wikiJar.each do |cookie|
-				response.set_cookie(cookie.name, value: cookie.value, domain: '.science.mcgill.ca' )
+			#response.set_cookie(cookie.name, value: cookie.value, domain: '.science.mcgill.ca' )
+			response.set_cookie(cookie.name, value: cookie.value )
 		end
 		gitblitJar.each do |cookie|
-				response.set_cookie(cookie.name, value: cookie.value, domain: '.mcgill.ca' )
+			#response.set_cookie(cookie.name, value: cookie.value, domain: '.mcgill.ca' )
+			response.set_cookie(cookie.name, value: cookie.value )
 		end
 
 		rstr = HTTP::Cookie.cookie_value(wikiJar.cookies($wikiUri))
 		erb :login, locals: {user: params[:user], tepidJson: tepidJson, tepidUserId: tepidUserId}
 	end
+end
+
+get '/logout' do
+	cookieStr = request.env['HTTP_COOKIE']
+	if cookieStr.nil?
+		halt 418, '急須です'
+	end
+	
+	cookies = HTTP::Cookie.cookie_value_to_hash(cookieStr)
+	cookies.each do |name, value|
+		response.set_cookie(name, value: value, expires: Time.new(1970))
+	end
+	erb :logout
+	
 end
 
